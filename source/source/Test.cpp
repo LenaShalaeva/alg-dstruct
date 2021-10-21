2841936fe21ca4a24afdc61b077c4502e7436c5d
LenaShalaeva <92307438+LenaShalaeva@users.noreply.github.com>
2021-10-21 23:25:13 +0300

    Убрала лишний main

--- source/source/Test.cpp
+++ source/source/Test.cpp
@@ -10,11 +10,6 @@ TEST(TestCaseName, TestName) {
 	EXPECT_TRUE(true);
 }
 
-int main(int argc, char* argv[]) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
-
 TEST(check_order_test, checking_abc_order)
 {
 	char s1[21] = "ab";
